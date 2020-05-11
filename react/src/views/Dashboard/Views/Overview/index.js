c1155b4885ce9f8c7449b90a00c2d2295b6c9f5a
Vahid <veyorokon@gmail.com>
2020-05-11 19:28:40 -0400

    Beginning layout.

--- react/src/views/Dashboard/Views/Overview/index.js
+++ react/src/views/Dashboard/Views/Overview/index.js
@@ -1,11 +1,25 @@
 import React from "react";
+// import Hero from "./Sections/Hero";
+import {Flex} from "components";
+import styled from "styled-components";
 
-import Hero from "./Sections/Hero";
+const LeftPanel = styled(Flex)`
+  width: 24rem;
+  flex-direction: column;
+`;
+const RightPanel = styled(Flex)`
+  width: 100%;
+  flex-direction: column;
+`;
 
+const Container = styled(Flex)`
+  position: relative;
+`;
 export default props => {
   return (
-    <>
-      <Hero />
-    </>
+    <Container>
+      <LeftPanel overflow="auto">left</LeftPanel>
+      <RightPanel overflow="auto">right</RightPanel>
+    </Container>
   );
 };

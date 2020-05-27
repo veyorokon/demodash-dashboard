187c784a13904596b563652c9c51d19ad3f4e1bb
Vahid <veyorokon@gmail.com>
2020-05-27 17:22:14 -0400

    Added button to product cards.

--- react/src/views/Dashboard/Panels/FindDemos/Results.js
+++ react/src/views/Dashboard/Panels/FindDemos/Results.js
@@ -3,6 +3,24 @@ import {Flex, Image, Text} from "components";
 import {responsive as r} from "lib";
 import styled from "styled-components";
 import bromane from "assets/images/bromane-brand.jpg";
+import {Button} from "components";
+
+export const CallToActionButton = styled(Button)`
+  height: ${props =>
+    props.height ? props.height : props.h ? props.h : "5rem"};
+  cursor: pointer;
+  min-width: fit-content;
+  border: none;
+  outline: none;
+  letter-spacing: 0.2px;
+  transition: all 0.3s ease-in-out;
+  text-transform: uppercase;
+
+  &:hover {
+    color: ${props => props.hoverColor || "black"};
+    background: ${props => props.hoverBg || "#f7d590"};
+  }
+`;
 
 const Card = styled(Flex)`
   border-bottom: 1px solid #dae0e6;
@@ -10,12 +28,32 @@ const Card = styled(Flex)`
   transition: all 0.1s;
 `;
 
+const Price = props => {
+  return (
+    <Flex flexGrow={0} h="fit-content" w={"100%"}>
+      <Text mr={2} letterSpacing="0.5px" color={"navys.1"} fw={500} w={"100%"}>
+        {props.title}
+      </Text>
+      {props.price ? (
+        <Text letterSpacing="0.5px" color={"navys.1"} fw={600} w={"100%"}>
+          ${props.price.toFixed(2)}
+        </Text>
+      ) : (
+        <Text letterSpacing="0.5px" color={"greens.4"} fw={600} w={"100%"}>
+          Free
+        </Text>
+      )}
+    </Flex>
+  );
+};
+
 const ImageCard = props => {
   return (
     <Card
       p={3}
-      maxWidth={r("26rem --> 33rem  22rem 23rem -> 30rem 24rem 30rem")}
-      mr={2}
+      maxWidth={r("26rem --> 33rem  22rem 23rem 31rem 34rem 24rem 30rem 27rem")}
+      mr={1}
+      ml={1}
       mt={3}
       mb={3}
       bg={"whites.0"}
@@ -33,86 +71,60 @@ const ImageCard = props => {
       >
         {props.title}
       </Text>
-      <Text
-        mt="auto"
-        letterSpacing="0.5px"
-        color={"navys.0"}
-        mb={2}
-        fw={300}
-        w={"100%"}
-      >
+      <Text letterSpacing="0.5px" color={"navys.0"} mb={2} fw={300} w={"100%"}>
         {props.description}
       </Text>
-      <Flex flexGrow={0} h="fit-content" mt="auto" w={"100%"}>
-        <Text
-          mr={2}
-          letterSpacing="0.5px"
-          color={"navys.2"}
-          fw={500}
-          w={"100%"}
-        >
-          Box price:
-        </Text>
-        {props.boxPrice ? (
-          <Text letterSpacing="0.5px" color={"navys.1"} fw={600} w={"100%"}>
-            ${props.boxPrice.toFixed(2)}
-          </Text>
-        ) : (
-          <Text letterSpacing="0.5px" color={"greens.4"} fw={600} w={"100%"}>
-            Free
-          </Text>
-        )}
-      </Flex>
-
-      <Flex flexGrow={0} h="fit-content" w={"100%"}>
-        <Text
-          mr={2}
-          letterSpacing="0.5px"
-          color={"navys.2"}
-          fw={500}
-          w={"100%"}
-        >
-          Box refill:
-        </Text>
-        {props.refillPrice ? (
-          <Text letterSpacing="0.5px" color={"navys.1"} fw={600} w={"100%"}>
-            ${props.refillPrice.toFixed(2)}
-          </Text>
-        ) : (
-          <Text letterSpacing="0.5px" color={"greens.4"} fw={600} w={"100%"}>
-            Free
-          </Text>
-        )}
-      </Flex>
+      <Price title={"Box price:"} value={props.boxPrice} />
+      <Price title={"Box refill:"} value={props.refillPrice} />
+      <Price title={"Shipping:"} value={props.shippingPrice} />
+      <CallToActionButton br={2} mt={3} bg={"yellows.1"} w="100%">
+        Become a demoer
+      </CallToActionButton>
     </Card>
   );
 };
 
 export default function Results(props) {
   return (
-    <Flex
-      boxShadow={0}
-      br={2}
-      bg="whites.0"
-      w={r("80rem ---------> 100rem")}
-      maxWidth="100%"
-      flexWrap={r("wrap")}
-      mb={4}
-      p={r("0 --> 3 -----> 4")}
-      justifyContent={"center"}
-    >
-      <ImageCard
-        title="Bromane - Hair filling fibers"
-        description="Hair filling fibers that add density to thinning hair"
-        boxPrice={1}
-        refillPrice={1}
-      />
-      <ImageCard
-        title="Bromane - Hair filling fibers - starter kit"
-        description="Hair filling fibers, an applicator pump and cleaning cloth"
-        boxPrice={0}
-        refillPrice={0}
-      />
-    </Flex>
+    <>
+      <Flex mb={4}>
+        <Text fw={500} fs={"2rem"}>
+          Demo boxes
+        </Text>
+      </Flex>
+      <Flex
+        boxShadow={0}
+        br={2}
+        bg="whites.0"
+        w={r("80rem ---------> 100rem")}
+        maxWidth="100%"
+        flexWrap={r("wrap")}
+        mb={4}
+        p={r("0 --> 3 -----> 4")}
+        justifyContent={"center"}
+      >
+        <ImageCard
+          title="Bromane - Hair filling fibers"
+          description="Hair filling fibers that add density to thinning hair"
+          boxPrice={1}
+          refillPrice={1}
+          shippingPrice={0}
+        />
+        <ImageCard
+          title="Bromane - Hair filling fibers - starter kit"
+          description="Hair filling fibers, an applicator pump and cleaning cloth"
+          boxPrice={0}
+          refillPrice={0}
+          shippingPrice={0}
+        />
+        <ImageCard
+          title="Bromane - Hair filling fibers - starter kit"
+          description="Hair filling fibers, an applicator pump and cleaning cloth"
+          boxPrice={0}
+          refillPrice={0}
+          shippingPrice={0}
+        />
+      </Flex>
+    </>
   );
 }

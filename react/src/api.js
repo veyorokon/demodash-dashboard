7a2d02766f1694b275600507aad3e911cbb461a1
Vahid <veyorokon@gmail.com>
2020-11-25 22:01:55 -0500

    Added bucket to api

--- react/src/api.js
+++ react/src/api.js
@@ -6,7 +6,8 @@ export const API_SERVER =
   `${process.env.REACT_APP_API_HOST}` || "http://192.168.200.238:8000";
 
 export const API_MEDIA =
-  `${process.env.REACT_APP_API_MEDIA}/media/` || API_SERVER.concat("/media/");
+  `${process.env.REACT_APP_API_MEDIA}/${process.env.REACT_APP_API_STATIC_BUCKET}/media/` ||
+  API_SERVER.concat("/media/");
 
 export const RECAPTCHA_PUBLIC_KEY =
   process.env.REACT_APP_RECAPTCHA_PUBLIC_KEY ||

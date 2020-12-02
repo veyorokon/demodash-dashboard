1aa72cd912bcf323c51dde08bb561235d427cd69
Vahid <veyorokon@gmail.com>
2020-12-02 16:00:19 -0500

    Separated form card

--- react/src/views/Dashboard/Panels/Home/Sections/ProfileForm/index.js
+++ react/src/views/Dashboard/Panels/Home/Sections/ProfileForm/index.js
@@ -1,3 +1,25 @@
-import ProfileForm from "./ProfileForm";
+import React from "react";
+import {Flex, Text} from "components";
+import AccountFormCard from "./AccountFormCard";
+import {responsive as r} from "lib";
 
+const ProfileForm = props => {
+  return (
+    <>
+      <Flex mb={4}>
+        <Text fw={500} fs={"2rem"}>
+          Profile
+        </Text>
+      </Flex>
+      <Flex
+        w={r("80rem ---------> 100rem")}
+        maxWidth="100%"
+        mb={4}
+        justifyContent="center"
+      >
+        <AccountFormCard title={"Account settings"} />
+      </Flex>
+    </>
+  );
+};
 export default ProfileForm;
